878b607a793c799599cebbcda398930d932fc912
Ba4eSenko <asenpopov8@gmail.com>
2026-01-18 17:40:10 +0200

    Rock Paper Scissors

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,29 +1,24 @@
 import random
-computer = random.randint(1,3)
+
 while True:
+    computer = random.randint(1, 3)
     player = input(" 1: Rock \n 2: Paper \n 3: Scissors   \n: ")
     numb = int(player)
     if numb == computer:
         print("Draw!")
         print("Would you like to play again? ")
         again = input()
-        if again == "yes" or again == "y" or again == "Y" or again == "Yes":
-            player = input(" 1: Rock \n 2: Paper \n 3: Scissors   \n: ")
-        if again == "no" or again == "n" or again == "No" or again == "NO":
+        if again != "yes" and again != "y" and again != "Y" and again != "Yes":
             break
     if numb == 1 and computer == 3 or numb == 2 and computer == 1 or numb == 3 and computer == 2:
         print("You win!")
         print("Would you like to play again? ")
         again = input()
-        if again == "yes" or again == "y" or again == "Y" or again == "Yes":
-            player = input(" 1: Rock \n 2: Paper \n 3: Scissors   \n: ")
-        if again == "no" or again == "n" or again == "No" or again == "NO":
+        if again != "yes" and again != "y" and again != "Y" and again != "Yes":
             break
     if numb == 1 and computer == 2 or numb == 2 and computer == 3 or numb == 3 and computer == 1:
         print("You lose!")
         print("Would you like to play again? ")
         again = input()
-        if again == "yes" or again == "y" or again == "Y" or again == "Yes":
-            player = input(" 1: Rock \n 2: Paper \n 3: Scissors   \n: ")
-        if again == "no" or again == "n" or again == "No" or again == "NO":
+        if again != "yes" and again != "y" and again != "Y" and again != "Yes":
             break
